f288f49add418a079673cad07b9e326c43ceb68e
Cristina <cristinalopez103@gmail.com>
2022-05-23 22:20:33 +0200

    Añado comentarios al archivo LoadData

--- database/LoadData.php
+++ database/LoadData.php
@@ -2,32 +2,31 @@
 include __DIR__ . "/conexion.php";
 
 class LoadData extends Connect {
-    public function __construct() {
-        parent::__construct();
+    public function __construct() { /* constructor de la clase LoadData */
+        parent::__construct();  /* instancia de la clase Connect */
     }
 
     /**
      * Carga las provincias en la base de datos.
-     * @param string $stringData Debe pasar como parámetro un string
-     * en formato JSON.
+     * @param string $stringData nombre que recibe la variable que tiene una cadena de texto en formato JSON.
      * 
      * @return bool
      */
-    public function pushProvinces(string $stringData): bool {
-        $data = json_decode($stringData);
+    public function pushProvinces(string $stringData): bool { /* devuelve true si losd atos cargaron correctamento si no devuelve false */
+        $data = json_decode($stringData); /* 'json_decode' convierte la cadena de texto en formato JSON en PHP */
 
         $pdo = $this->getPDO();
         $stmt = $pdo->prepare('INSERT INTO dl_province(`province_name`) values(:province_name)');
 
-        $pdo->beginTransaction();
+        $pdo->beginTransaction(); /* ayuda a que los datos carguen rápidamente y no tarde en completar la carga de los datos */
 
-        foreach($data as $key => $value) {
+        foreach($data as $key => $value) { /* sentencia que hace enviar los datos a la tabla */
             $stmt->execute([
                 ':province_name' => $value->province
             ]);
         }
 
-        return $pdo->commit();
+        return $pdo->commit(); /* funcion que se ejecuta cuando se termine de ejecutar 'foreach' y envia todos los datos a la tabla provincia */
     }
 
     /**
@@ -88,7 +87,7 @@ class LoadData extends Connect {
 exit;
 
 /**
- * @var \LoadData Objeto que permite cargar los datos en la base
- * de datos a partir de un JSON.
+ * @var \LoadData clase que tiene una serie de métodos que permite cargar los datos en formato JSON 
+ * en la base de datos MySQL.
  */
 $loadData = new LoadData;
